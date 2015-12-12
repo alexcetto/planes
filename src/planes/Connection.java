f110ddbad8d6283373fe1b2d8284197056d553d3
alexandrecetto <alexandrecetto@gmail.com>
2015-12-12 11:16:53 +0100

    Ajout d'un test de connexion à main Ajout d'une classe User (vide pour l'instant) Connexion --> GlobalConnector Ajout de la lib mariaDB pour la connexion SQL Update du gitignore pour virer les fichiers inutiles Suppression de salut.html

--- src/planes/Connection.java
+++
@@ -1,5 +0,0 @@
-package planes;
-
-public class Connection {
-
-}
---
+++ src/planes/User.java
@@ -0,0 +1,11 @@
+package planes;
+
+/**
+ * Created by alexandrecetto on 12/12/2015.
+ */
+public class User {
+    int id;
+    String name;
+    Byte mdp;
+
+}
