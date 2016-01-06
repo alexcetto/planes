d762adeb36b1bf7ed52d6da4040873a59886ae39
FAVEZ Adrien <adrien.favez@gmail.com>
2016-01-06 17:40:31 +0100

    Menuing Admin suite

--- src/planes/Main.java
+++ src/planes/Main.java
@@ -11,39 +11,58 @@ public class Main {
 	}
 	
 	public static int menuAdmin(){
+		int choice = 0, choice2 = 0;
 		
-		int choice;
-		
-		System.out.println("\n\n	---	Main Admin Menu	---\n");
-		System.out.println("1 - Manage database");
-		System.out.println("2 - Manage users");
-		System.out.println("3 - Deconnection");
-		System.out.println("4 - Quit");
-		System.out.print("\nYour choice : ");
-		choice = sc.nextInt();
-		
-		switch(choice){
-			case 1:
-				System.out.println("\n\n	---	Database Admin Menu	---\n");
-				break;
-			case 2:
-				System.out.println("\n\n	---	Users Admin Menu	---\n");
-				break;
-			case 3:
-			case 4:
-			default:
-				return choice;	
+		boolean main = true, second = true;
+		while(main){
+			main = false;
+			
+			System.out.println("\n\n	---	Main Admin Menu	---\n");
+			System.out.println("1 - Manage database");
+			System.out.println("2 - Manage users");
+			System.out.println("3 - Deconnection");
+			System.out.println("4 - Quit");
+			System.out.print("\nYour choice : ");
+			choice = sc.nextInt();
+			
+			switch(choice){
+				case 1:
+					System.out.println("\n\n	---	Database Admin Menu	---\n");
+					break;
+				case 2:
+					System.out.println("\n\n	---	Users Admin Menu	---\n");
+					break;
+				case 3:
+				case 4:
+					return choice;
+				default:
+					main = true;	
+			}
 		}
 		
 		choice *= 10;
-		System.out.println("1 - Add");
-		System.out.println("2 - Change");
-		System.out.println("3 - Delete");
-		System.out.println("4 - Back");
-		System.out.print("\nYour choice : ");
-		choice += sc.nextInt();
-		
-		if(choice==14 || choice==24) choice = menuAdmin();
+		while(second){
+			second = false;
+			System.out.println("1 - Add");
+			System.out.println("2 - Change");
+			System.out.println("3 - Delete");
+			System.out.println("4 - Back");
+			System.out.print("\nYour choice : ");
+			choice2 = sc.nextInt();
+			
+			switch(choice2){
+				case 1:
+				case 2:
+				case 3:
+					choice += choice2;
+					break;
+				case 4:
+					choice = menuAdmin();
+					break;
+				default:
+					second = true;
+			}
+		}
 		
 		return choice;
 	}
@@ -68,12 +87,13 @@ public class Main {
 						case 4:
 							runMain = false;
 						case 3:
-						default:
 							runUser = false;
+							break;
+						default:
 					}
 				}else{
 					
-					
+					System.out.println("Sorry User but your process is in another time");
 				}
 			}	
 		}
