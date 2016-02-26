8240f43258d3839110c144f1a4583427ac4d2267
alexandrecetto <alexandrecetto@gmail.com>
2016-02-26 09:15:18 +0100

    Ajout du critère Aircraft Type et mise à jour de toutes les classes. Ajout dans l'IHM d'aircraft type IHM - Fenêtre un peu plus grande
    @TODO :
    Ajouter les dernières input pour l'IHM (capacité, prix à implémenter)
    Faire la partie Admin

--- src/planes/User/GlobalConnector.java
+++ src/planes/User/GlobalConnector.java
@@ -20,9 +20,9 @@ public class GlobalConnector {
 	            System.out.println("e = " + e);
 	        }
 	        Connection conn = null;
-	        String url = "jdbc:mysql://mysql5-public.web4all.fr:3306/133348_planes";
-	        String utilisateur = "133348_planes";
-	        String motDePasse = "I like to fly !";
+	        String url = "jdbc:mysql://thedatabasewebsite.Com:3306/nameofthedatabase";
+	        String utilisateur = "username";
+	        String motDePasse = "password";
 	        try {
 	            conn = DriverManager.getConnection( url, utilisateur, motDePasse );
 	            //if(conn != null) System.out.println("Connexion établie : "+conn);
